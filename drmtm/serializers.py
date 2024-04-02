32f4cde900efb3d847b7cd77c295a431e57422de
Daniel Ji <daniel.ji0923@gmail.com>
2024-04-02 14:41:55 -0700

    added id to serializer

--- drmtm/serializers.py
+++ drmtm/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class DrmtmSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Drmtm
-        fields=('name', 'position', 'age', 'height', 'salary')
+        fields=('name', 'position', 'age', 'height', 'salary', 'id')
